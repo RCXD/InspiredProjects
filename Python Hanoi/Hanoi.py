69bf51fd45f144e1d52c7cf81716df291001f82b
RCXD <tpxdkss@gmail.com>
2021-02-03 05:50:22 -0600

    Bugfix, add time complexity

--- Python Hanoi/Hanoi.py
+++ Python Hanoi/Hanoi.py
@@ -1,6 +1,6 @@
 # Question: If I have n disks, move disks into designated peg.
 # Rule 01: Larger disk cannot be on top of the smaller disk
-# Rule 02: Should move the disk on top first
+# Rule 02: Should move the disk on top first, one by one
 
 '''
     peg i           peg j           peg k
@@ -10,7 +10,6 @@
     D
     E
 '''
-MAX_SIZE = 500 #Virtual limit
 
 # Attempt 01: Stack
 class Node:
@@ -26,14 +25,14 @@ class Stack:
         print('created peg', self.name)
 
     def push(self, data):
-        if not self.isFull():
+        #if not self.isFull():
             newNode=Node(data)
             self.stackList.insert(self.tempTop, newNode)
             self.top += 1
             self.tempTop += 1
             #print('pushed '+data)
-        else:
-            print('stack overflow')
+        # else:
+        #     print('stack overflow')
 
     def pop(self):
         if not self.isEmpty():
@@ -48,15 +47,15 @@ class Stack:
     def isEmpty(self):
         return self.tempTop == 0
 
-    def isFull(self):
-        return self.top==MAX_SIZE
+    # def isFull(self):
+    #     return self.top==MAX_SIZE
 
 def directMove(origin, dest):
     #origin.Stack()
     #dest.Stack()
     target = origin.pop()
-    dest.push(target) #되나? 됨!
-    print('moved', str(target), 'from', origin.name, 'to', dest.name)
+    dest.push(target) #works!
+    #print('moved', str(target), 'from', origin.name, 'to', dest.name)
 
 def move(n, i, j, k):
     if n==1:
@@ -67,31 +66,35 @@ def move(n, i, j, k):
         move(n-1, j, i, k) 
 
 #Setting up
+import time
+
 ii=Stack('i')
 jj=Stack('j')
 kk=Stack('k')
 num=int(input('Insert how many disks you have\n'))
 for i in range(0,num):
     #ii.push(chr(ord('E')-i))
-    ii.push('-'*(num-i))
-    print('pushed '+chr(ord('E')-i)+' in peg '+ii.name)    
-
+    ii.push(' '*i+'-'*(2*(num-i)-1))
+    #print('pushed '+chr(ord('E')-i)+' in peg '+ii.name)    
+    print('pushed '+' '*i+'-'*(2*(num-i)-1)+' in peg '+ii.name) 
+#Setup check(direct access to List)
 print(ii.name, jj.name, kk.name, sep='\t')
 for i in range(len(ii.stackList)):
     print(ii.stackList[len(ii.stackList)-i-1].data)
 
 #Solve problem
+begin=time.time()
 move(num, ii, jj, kk)
+end=time.time()
 
 #Show Status
 print(ii.name, jj.name, kk.name, sep='\t')
 for i in range(num):
     print(ii.pop(), jj.pop(), kk.pop(), sep='\t')
-
-
-# Attempt 02: Linear Linked List
-
+print(f"Total runtime of the movement is {end - begin}")
 
 # Other References
 # Python recurrsion: https://lucete1230-cyberpolice.tistory.com/222
 # Python ASCII: https://www.programiz.com/python-programming/examples/ascii-character
+# Python time module: https://www.geeksforgeeks.org/python-measure-time-taken-by-program-to-execute/
+# Python time complexity: https://wiki.python.org/moin/TimeComplexity
